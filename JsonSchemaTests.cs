87c8764a43013dc343854f31400f082f1f880d50
roopkt <roopkt@gmail.com>
2017-03-03 13:59:55 -0500

    created json schema tests project

---
+++ JsonSchemaTests.cs
@@ -0,0 +1,59 @@
+﻿
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json.Schema;
+using Xunit;
+
+
+namespace sample
+{
+    public class JsonSchemaTests
+    {
+        [Fact]
+        public void Validate_Person()
+        {
+            JsonSchema schema = JsonSchema.Parse(
+                @"{
+    'type': 'object',
+    'properties': {
+        'name': {'type':'string'},
+        'hobbies': {'type': 'array'}
+    },
+    'additionalProperties': false
+    }");
+          var person = JObject.Parse(@"{'name': 'James','hobbies': ['.NET', 'LOLCATS']}");
+            Assert.True(person.IsValid(schema));
+        }
+
+        [Fact]
+        public void Validate_Bad_Person()
+        {
+            JsonSchema schema = JsonSchema.Parse(
+                @"{
+    'type': 'object',
+    'properties': {
+        'name': {'type':'string'},
+        'hobbies': {'type': 'array'}
+    },
+    'additionalProperties': false
+    }");
+            var person = JObject.Parse(@"{'surname': 'James','hobbies': ['.NET', 'LOLCATS']}");
+            Assert.False(person.IsValid(schema));
+        }
+
+        [Fact]
+        public void Validate_Bad_Person_Property_Type()
+        {
+            JsonSchema schema = JsonSchema.Parse(
+                @"{
+    'type': 'object',
+    'properties': {
+        'name': {'type':'string'},
+        'hobbies': {'type': 'array'}
+    },
+    'additionalProperties': false
+    }");
+            var person = JObject.Parse(@"{'name': 2,'hobbies': ['.NET', 'LOLCATS']}");
+            Assert.False(person.IsValid(schema));
+        }
+    }
+}
