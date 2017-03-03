cc82c17cf860e701289bb2dc603e3635d216fd36
roopkt <roopkt@gmail.com>
2017-03-03 16:53:08 -0500

    updated tests

--- src/Tests/GenerateJsonSchemaTests.cs
+++ src/Tests/GenerateJsonSchemaTests.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections.Generic;
-using Newtonsoft.Json;
-using NJsonSchema;
+﻿using NJsonSchema;
 using NJsonSchema.CodeGeneration.TypeScript;
 using sample.Models;
 using Xunit;
@@ -22,42 +20,11 @@ namespace sample.Tests
         [Fact]
         public void I_Can_Generate_Typescript_Person()
         {
-           
             var schema = GetPersonJsonSchema();
-
-
             var generator = new TypeScriptGenerator(schema);
-            
-            var code = generator.GenerateFile();
-            
-            Assert.NotNull(code);
-
             var type = generator.GenerateType("person");
-
-             Assert.NotNull(type);
+            Assert.NotNull(type);
         }
-
-       
-
-        [Fact]
-        public void I_Can_Validate_P_Against_Schema()
-        {
-            var schema = GetPersonJsonSchema();
-
-            var person = new Person
-            {
-                Name = "Adam",
-                Age = 12,
-                Address = new Address { AddressLines = new List<AddressLine> { new AddressLine { Line = "Cool Street" } } }
-            };
-
-            var json = JsonConvert.SerializeObject(person);
-
-            var validations = schema.Validate(json);
-
-        }
-
-
 
         private static JsonSchema4 GetPersonJsonSchema()
         {
